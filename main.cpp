1dc9efaf851b69c1a1ef3f471d960e1f061329cd
AndreiTimofte96 <andrei.timofte96@gmail.com>
2016-12-11 01:56:38 +0200

    Tema2_IP gata
    8,475p

--- main.cpp
+++ main.cpp
@@ -1,29 +1,25 @@
-#include <iostream>
 #include "t2.h"
 
-using namespace std;
-
-int lengthCod = 4;
-int cod[5] = { -1, 0, 1, 2 }; //T A C G
+//int lengthCod=4;
+int cod[5] = {-1, 0, 1, 2}; //T A C G
 long long pow4[MAX_ADN] = { 1, 4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456, 1073741824, 4294967296, 17179869184, 68719476736, 274877906944, 1099511627776, 4398046511104, 17592186044416, 70368744177664, 281474976710656, 1125899906842624, 4503599627370496, 18014398509481984, 72057594037927936, 288230376151711744, 1152921504606846976, 4611686018427387904 };
-
 
 int solution[MAX_ADN], sum;
 
 void write(int k, char sirADN[MAX_ADN])
 {
 	int i;
-	for (i = 0; i<k; i++)
+	for (i=0; i<k; i++)
 	{
-		if (solution[i] == -1) sirADN[k-1-i] = 'T';
-		if (solution[i] == 0)  sirADN[k-1-i] = 'A';
-		if (solution[i] == 1)  sirADN[k-1-i] = 'C';
-		if (solution[i] == 2)  sirADN[k-1-i] = 'G';
+		if (solution[i] == -1) sirADN[k-1-i]='T';
+		if (solution[i] == 0)  sirADN[k-1-i]='A';
+		if (solution[i] == 1)  sirADN[k-1-i]='C';
+		if (solution[i] == 2)  sirADN[k-1-i]='G';
 	}
 	sirADN[k]='\0';
 }
 
-void bkt(int k, long long nrIntreg, char sirADN[MAX_ADN])
+/*void bkt(int k, long long nrIntreg, char sirADN[MAX_ADN])
 {
 	int i;
 
@@ -45,12 +41,17 @@ void bkt(int k, long long nrIntreg, char sirADN[MAX_ADN])
 		bkt(k + 1, nrIntreg, sirADN);
 		sum -= cod[i] * pow4[k];
 	}
-}
+}*/
 
 void codificareADN(long long nrIntreg, char sirADN[MAX_ADN])
 {
 	//bkt(0, nrIntreg, sirADN);
+
 	long long k=0, rest, cat, ok;
+
+	if (nrIntreg == 0)
+        solution[k++]=0;
+
 	if (nrIntreg>0)
     {
         while (nrIntreg)
@@ -72,7 +73,8 @@ void codificareADN(long long nrIntreg, char sirADN[MAX_ADN])
                         solution[k++]=nrIntreg-cat*4;
                         ok=1;
                     }
-            if(!ok )
+
+            if (!ok)
                 nrIntreg=nrIntreg/4;
             else
                 nrIntreg=cat;
@@ -102,20 +104,21 @@ void codificareADN(long long nrIntreg, char sirADN[MAX_ADN])
                             solution[k++]=nrIntreg-cat*4;
                             ok=1;
                         }
-            if(!ok )
+            if (!ok)
                 nrIntreg=nrIntreg/4;
             else
                 nrIntreg=cat;
         }
     }
+
     write(k, sirADN);
 }
 
 long long decodificareADN(char sirADN[MAX_ADN])
 {
- 	long long lg, k = 0, number=0;
+ 	long long lg, k=0, number=0;
 
-	for (lg = strlen(sirADN) - 1; lg >= 0; lg--)
+	for (lg=strlen(sirADN)-1; lg>=0; lg--)
 	{
 
 		if (sirADN[lg] == 'T') number += pow4[k++] * (-1);
@@ -140,19 +143,10 @@ void scadereADN(char primulSir[MAX_ADN], char alDoileaSir[MAX_ADN], char diferen
     codificareADN(dif, diferenta);
 }
 
-int main()
+rezultat mutariJoc(cartonas vecCartonase[MAX_CARTONASE], unsigned int nCartonase, char literaStart, char sirFinal[MAX_SIR])
 {
-	char ADN1[MAX_ADN], ADN2[MAX_ADN], ADN3[MAX_ADN];
-	codificareADN(28, ADN1);//a
-	cout<<ADN1<<'\n';
-	cout<<decodificareADN(ADN1)<<'\n';//b
-
-	codificareADN(1, ADN2);//a
-	cout<<ADN2<<'\n';
-	cout<<decodificareADN(ADN2)<<'\n';
-
-	//adunareADN(ADN1, ADN2, ADN3);
-	scadereADN(ADN1, ADN2, ADN3);
-	cout<<ADN3<<'\n';
-	return 0;
+    rezultat X;
+    X.sePoate=true;
+    X.nrMutari=0;
+    return X;
 }
